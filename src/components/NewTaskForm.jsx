d48112e5c7f5864ef06e5b1de96650f5f39208fe
Madi Rei <madison.nicole.rei@gmail.com>
2025-12-19 11:34:57 -0800

    refactors NewTaskForm and adds input validation

--- src/components/NewTaskForm.jsx
+++ src/components/NewTaskForm.jsx
@@ -1,35 +1,52 @@
 import { useState } from 'react';
 import PropTypes from 'prop-types';
 
+const kDefaultsFormState = {
+  title: '',
+  description: ''
+};
+
 const NewTaskForm = ({onHandleSubmit}) => {
-  const [title, setTitle] = useState('');
-  const [description, setDescription] = useState('');
+  const [formData, setFormData] = useState(kDefaultsFormState);
 
-  const handleTitleChange = (event) => {
-    setTitle(event.target.value);
-  };
+  const handleChange = (event) => {
+    const inputValue = event.target.value;
+    const inputName = event.target.name;
 
-  const handleDescriptionChange = (event) => {
-    setDescription(event.target.value);
+    setFormData(formData => {
+      return {
+        ...formData,
+        [inputName]: inputValue
+      };
+    });
   };
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    const newTask = {
-      title,
-      description,
-    };
-    onHandleSubmit(newTask);
-    setTitle('');
-    setDescription('');
+    onHandleSubmit(formData);
+    setFormData(kDefaultsFormState);
+  };
+
+  const makeControlledInput = (inputName) => {
+    return (
+      <input
+        type="text"
+        name={inputName}
+        id={`input-${inputName}`}
+        value={formData[inputName]}
+        onChange={handleChange}
+      />
+    );
   };
 
   return (
     <form onSubmit={handleSubmit}>
       <label htmlFor="title">Task Title:</label>
-      <input type="text" id="title" value={title} onChange={handleTitleChange}/>
+      { makeControlledInput('title') }
+      <input type="text" id="title" value={formData.title} onChange={handleChange}/>
       <label htmlFor="description">Description:</label>
-      <input type="text" id="description" value={description} onChange={handleDescriptionChange}/>
+      { makeControlledInput('description') }
+      <input type="text" id="description" value={formData.description} onChange={handleChange}/>
       <div>
         <input type="submit" value="Add a task" />
       </div>
