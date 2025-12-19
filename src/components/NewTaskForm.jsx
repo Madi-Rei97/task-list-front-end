1aeadef586903052d819e9da39d8e01b9372b8e0
Xi <youngjoycewu@gmail.com>
2025-12-20 01:47:42 +0800

    add on description for task form

--- src/components/NewTaskForm.jsx
+++ src/components/NewTaskForm.jsx
@@ -3,25 +3,33 @@ import PropTypes from 'prop-types';
 
 const NewTaskForm = ({onHandleSubmit}) => {
   const [title, setTitle] = useState('');
+  const [description, setDescription] = useState('');
 
   const handleTitleChange = (event) => {
     setTitle(event.target.value);
+  };
+
+  const handleDescriptionChange = (event) => {
+    setDescription(event.target.value);
   };
 
   const handleSubmit = (event) => {
     event.preventDefault();
     const newTask = {
       title,
-      isComplete: false,
+      description,
     };
     onHandleSubmit(newTask);
     setTitle('');
+    setDescription('');
   };
 
   return (
     <form onSubmit={handleSubmit}>
       <label htmlFor="title">Task Title:</label>
-      <input type="text" id="title" title="title" value={title} onChange={handleTitleChange}/>
+      <input type="text" id="title" value={title} onChange={handleTitleChange}/>
+      <label htmlFor="description">Description:</label>
+      <input type="text" id="description" value={description} onChange={handleDescriptionChange}/>
       <div>
         <input type="submit" value="Add a task" />
       </div>
